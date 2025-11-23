2149383f3996139e3e97b4b2042a4f759a9a2a03
Vicillian <44278914+Vicillian@users.noreply.github.com>
2025-11-22 16:04:42 -0800

    Add test function for animal-highlight game
    Added a test function 'blahblu' for game logic.

--- animal-highlight.js
+++ animal-highlight.js
@@ -1,2 +1,12 @@
 // Test TEst
 // Here's the JS for the animal-highlight game
+
+function blahblu(blahblah) {
+//test function to build basis of lamp over bug code
+    let lampHoverBug = false;
+    if (userNumber == whatNumber) {
+      $("#output").html("You got it!");
+    }   else {
+      $("#output").html("Noooooope");
+    }
+};
