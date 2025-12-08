35108943fa88c3eb051e616b9a82d86f70e1185b
liyuan29 <sashajarcadia@gmail.com>
2025-12-08 15:47:31 -0800

    Implement drag-and-drop for lamp wrapper
    Added dragging functionality for the lamp wrapper.

--- flippingstones.js
+++ flippingstones.js
@@ -67,10 +67,39 @@ for (let i = 0; i < rocks.length; i++) {
     }
 }
 
+
     function unlockDoor() {
         
        
         
         
         gameEnded = true;
+
+
 }
+
+$(function () {
+  let isDragging = false;
+  let offsetX, offsetY;
+
+  $("#lampWrapper").on("mousedown", function (e) {
+    isDragging = true;
+    offsetX = e.clientX - $(this).position().left;
+    offsetY = e.clientY - $(this).position().top;
+    $(this).css("cursor", "grabbing");
+  });
+
+  $(document).on("mousemove", function (e) {
+    if (isDragging) {
+      $("#lampWrapper").css({
+        left: e.clientX - offsetX,
+        top: e.clientY - offsetY
+      });
+    }
+  });
+
+  $(document).on("mouseup", function () {
+    isDragging = false;
+    $("#lampWrapper").css("cursor", "grab");
+  });
+});
